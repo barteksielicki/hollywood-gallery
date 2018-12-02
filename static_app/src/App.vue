7a00a294f42c8af380830d54f281335c44c2609f
Damian Kisielinski <damian.kisielinski@gmail.com>
2018-12-02 13:40:02 +0100

    testy

--- static_app/src/App.vue
+++ static_app/src/App.vue
@@ -1,15 +1,14 @@
 <template>
   <div id="app">
     <div>
-      <h1>Face detector</h1>
-      <img src="https://via.placeholder.com/480x360" v-if="!cameraLoaded">
-      <video id="video" width="480" height="360" preload autoplay loop muted v-show="cameraLoaded"></video>
+      <h1>File uploader</h1>
+      <input type="file" @change="onFileChanged">
+	  <button @click="onUpload">Upload!</button>
     </div>
-    <div>
-      <h1>Face processor</h1>
-      <img id="input-image" :src="this.base64Image || 'https://via.placeholder.com/480x360'">
-      <img id="output-image" :src="this.responseImage || 'https://via.placeholder.com/480x360'">
-      <h3 class="text-center">{{ actorName }}</h3>
+    <div v-for="image in images">
+      <img id="input-image" :src="image.base64Image || 'https://via.placeholder.com/240x180'">
+      <img id="output-image" :src="image.responseImage || 'https://via.placeholder.com/240x180'">
+      <h3 class="text-center">{{ image.actorName }}</h3>
     </div>
   </div>
 </template>
@@ -17,7 +16,7 @@
 <script>
   import axios from 'axios'
 
-  const API_URL = ''
+  const API_URL = 'http://3.121.199.45'
 
   export default {
     data () {
@@ -25,12 +24,11 @@
         video: null,
         canvas: null,
         context: null,
-        blobImage: null,
-        base64Image: null,
-        responseImage: null,
-        actorName: null,
+        images: [],
+        tmpImage: null,
         modelLoaded: false,
         cameraLoaded: false,
+        selectedFile: null
       }
     },
     computed: {
@@ -66,64 +64,34 @@
           console.log("Error loading model.", err)
         })
     },
-    watch: {
-      isReady (nowReady, wasReady) {
-        if (!wasReady && nowReady) {
-          console.log("Detecting faces started...")
-          setTimeout(this.detectFace, 1000)
-        }
-      },
-      blobImage (blob) {
-        // render preview
-        const reader = new FileReader();
-        reader.readAsDataURL(blob)
+    methods: {
+	  onFileChanged (event) {
+    	this.selectedFile = event.target.files[0]
+    	const reader = new FileReader();
+        reader.readAsDataURL(this.selectedFile)
         reader.onloadend = () => {
-          this.base64Image = reader.result
+          this.tmpImage = {
+          	base64Image: reader.result
+          };
         }
-        // send and retrieve result
-        const data = new FormData()
-        data.append('photo', blob)
-        axios.post(API_URL + '/process', data, {
+	  },
+	  onUpload() {
+	    const data = new FormData()
+	    data.append('photo', this.selectedFile)
+	    axios.post(API_URL + '/process', data, {
           headers: {'Content-Type': 'multipart/form-data'}
         }).then(response => {
           console.log(response)
           if (response.data.photo) {
-            this.responseImage = `${API_URL}/${response.data.photo}`
-            this.actorName = response.data.name
+            this.tmpImage["responseImage"] = `${API_URL}/${response.data.photo}`
+            this.tmpImage["actorName"] = response.data.name
+            this.images.push(this.tmpImage)
           } else {
-            alert("Backend couldn't find face!")
             this.responseImage = null
             this.actorName = null
           }
         })
-      }
-    },
-    methods: {
-      detectFace () {
-        console.log("detecting...")
-        faceapi.detectSingleFace(this.video)
-          .then(result => {
-            if (result && result.score > 0.5) {
-              console.log("Face deteceted.")
-              this.context.drawImage(this.video, 0, 0, this.video.width, this.video.height)
-              this.canvas.toBlob(blob => {
-                this.blobImage = blob
-              })
-              setTimeout(this.detectFace, 5000)
-            } else {
-              console.log("Face was not detected.")
-              this.blobImage = null
-              this.base64Image = null
-              this.responseImage = null
-              this.actorName = null
-              setTimeout(this.detectFace, 1000)
-            }
-          })
-          .catch(error => {
-            console.log("Error detecting face.", error)
-            setTimeout(this.detectFace, 1000)
-          })
-      }
+	  }
     }
   }
 </script>
@@ -157,7 +125,7 @@
   }
 
   img {
-    width: 480px;
-    height: 360px;
+    width: 240px;
+    height: 180px;
   }
 </style>
