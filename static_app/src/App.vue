8de860313985ab45114ccdfe4ca906ffffd5a881
barteksielicki <barteksielicki@gmail.com>
2018-12-29 12:13:59 +0100

    Stop using faceapi to detect face on frontend side

--- static_app/src/App.vue
+++ static_app/src/App.vue
@@ -56,7 +56,6 @@
         base64Image: null,
         responseImage: null,
         actorName: null,
-        modelLoaded: false,
         cameraLoaded: false,
         actorsLimit: 1000,
         actors_history: [],
@@ -67,7 +66,7 @@
     },
     computed: {
       isReady () {
-        return this.modelLoaded && this.cameraLoaded
+        return this.cameraLoaded
       }
     },
     mounted () {
@@ -77,85 +76,64 @@
       navigator.mediaDevices.getUserMedia({video: true, audio: false})
         .then(stream => {
           this.video.srcObject = stream
-          this.cameraLoaded = true
-          this.canvas = document.createElement('canvas');
-          this.canvas.width = this.video.width;
-          this.canvas.height = this.video.height;
+          this.canvas = document.createElement('canvas')
+          this.canvas.width = this.video.width
+          this.canvas.height = this.video.height
           this.context = this.canvas.getContext('2d')
-          console.log("Camera loaded")
+          this.cameraLoaded = true
+          console.log('Camera loaded')
         })
         .catch(error => {
-          console.log("Error loading camera.", error)
-        })
-
-      // load network model
-      faceapi.loadSsdMobilenetv1Model('dist/')
-        .then(() => {
-          this.modelLoaded = true
-          console.log("Model loaded")
-        })
-        .catch(err => {
-          console.log("Error loading model.", err)
+          console.log('Error loading camera.', error)
         })
     },
     watch: {
       isReady (nowReady, wasReady) {
         if (!wasReady && nowReady) {
-          console.log("Detecting faces started...")
-          setTimeout(this.detectFace, 1000)
+          console.log('Capturing loop started...')
+          setTimeout(this.captureFrame, 1000)
         }
       },
       blobImage (blob) {
         // render preview
-        const reader = new FileReader();
+        const reader = new FileReader()
         reader.readAsDataURL(blob)
         reader.onloadend = () => {
           this.base64Image = reader.result
         }
+
         // send and retrieve result
         const data = new FormData()
         data.append('photo', blob)
         data.append('limit', this.actorsLimit)
+        console.log('making request...')
         axios.post(API_URL + '/process', data, {
           headers: {'Content-Type': 'multipart/form-data'}
         }).then(response => {
-          console.log(response)
           if (response.data.photo) {
             this.responseImage = `${API_URL}/${response.data.photo}`
             this.actorName = response.data.name
-            this.save_stats(response.data.name);
+            this.save_stats(response.data.name)
           } else {
             this.responseImage = null
             this.actorName = null
           }
+        }).catch(err => {
+          this.responseImage = null
+          this.actorName = null
+        }).finally(() => {
+          setTimeout(this.captureFrame, 1000)
         })
       }
     },
     methods: {
-      detectFace () {
-        console.log("detecting...")
-        faceapi.detectSingleFace(this.video)
-          .then(result => {
-            if (result && result.score > 0.5) {
-              console.log("Face deteceted.")
-              this.context.drawImage(this.video, 0, 0, this.video.width, this.video.height)
-              this.canvas.toBlob(blob => {
-                this.blobImage = blob
-              })
-              setTimeout(this.detectFace, 2000)
-            } else {
-              console.log("Face was not detected.")
-              this.blobImage = null
-              this.base64Image = null
-              this.responseImage = null
-              this.actorName = null
-              setTimeout(this.detectFace, 1000)
-            }
-          })
-          .catch(error => {
-            console.log("Error detecting face.", error)
-            setTimeout(this.detectFace, 1000)
-          })
+      captureFrame () {
+        // capture camera frame
+        console.log('capturing frame...')
+        this.context.drawImage(this.video, 0, 0, this.video.width, this.video.height)
+        this.canvas.toBlob(blob => {
+          this.blobImage = blob
+        })
       },
       save_stats (name) {
         this.n++
