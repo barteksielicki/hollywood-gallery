28471a231a6ae126781e84615d02530512deb4b1
barteksielicki <barteksielicki@gmail.com>
2019-01-24 23:13:12 +0100

    add footer and names

--- app/ui/src/App.vue
+++ app/ui/src/App.vue
@@ -11,6 +11,14 @@
         </div>
       </div>
       <div>
+        <h1>Settings</h1>
+        <div>
+          Actors limit: {{ actorsLimit }}<br>
+          <input type="range" min="1" max="18827" v-model="actorsLimit" class="slider"><br>
+          <button @click="rotate">Rotate camera input</button>
+        </div>
+      </div>
+      <div>
         <h1>Face detector</h1>
         <div class="video-wrapper">
           <img class="placeholder-img" src="https://via.placeholder.com/480x480" v-if="!cameraLoaded">
@@ -30,19 +38,24 @@
         <canvas id="frame-canvas" width="240" height="240"></canvas>
         <img class="classification-img" id="output-image"
              :src="this.responseImage || 'https://via.placeholder.com/240x240'">
-      </div>
-      <div>
-        <h1>Settings</h1>
-        <div>
-          Actors limit: {{ actorsLimit }}<br>
-          <input type="range" min="1" max="18826" v-model="actorsLimit" class="slider"><br>
-          <button @click="rotate">Rotate camera input</button>
-        </div>
+        <br>
+        <p>{{ this.responseName }}</p>
       </div>
       <hr>
       <div class="history">
-        <img v-for="image in history" :src="image">
+        <h2>Last 5 actors</h2>
+        <div class="history-item" v-for="actor in history">
+          <img :src="actor.image"><br>
+          <p>{{ actor.name }}</p>
+        </div>
       </div>
+      <hr>
+      <footer>
+        <p>
+          <small>&copy; 2019 B. Sielicki, P. Wołosz, D. Kisieliński</small><br>
+          <a href="https://github.com/barteksielicki/hollywood-gallery">Warsztaty badawcze</a>
+        </p>
+      </footer>
     </div>
   </div>
 </template>
@@ -61,6 +74,7 @@
         blobImage: null,
         base64Image: null,
         responseImage: null,
+        responseName: '',
         cameraLoaded: false,
         actorsLimit: 1000,
         history: [],
@@ -106,6 +120,7 @@
         // // }
 
         const currentImage = this.responseImage
+        const currentName = this.responseName
 
         // send and retrieve result
         const data = new FormData()
@@ -117,9 +132,11 @@
         }).then(response => {
           if (response.data.photo) {
             this.responseImage = `${API_URL}/${response.data.photo.replace('data/imdb_crop', 'img')}`
+            this.responseName = response.data.name
             this.faceNotDetectedCounter = 0
           } else {
             this.responseImage = null
+            this.responseName = ''
             this.showWarn('Cannot detect face', 'Try to bring your face closer to the webcam.')
             if (this.faceNotDetectedCounter++ === 10) {
               this.resetStats()
@@ -127,13 +144,17 @@
           }
         }).catch(err => {
           this.responseImage = null
+          this.responseName = ''
           this.showError('Error', 'Something went wrong, please contact serwer administrator.')
         }).finally(() => {
           if (currentImage && this.history.length === 5) {
             this.history.pop()
           }
           if (currentImage) {
-            this.history.unshift(currentImage)
+            this.history.unshift({
+              image: currentImage,
+              name: currentName
+            })
           }
           setTimeout(this.captureFrame, 1000)
         })
@@ -284,13 +305,26 @@
   .history {
     text-align: left;
     padding-left: 5%;
-    img {
+    .history-item {
+      display: inline-block;
       width: 15%;
 
-      height: auto;
+
       &:not(:last-child) {
         margin-right: 5%;
       }
+    }
+    img {
+      width: 100%;
+      height: auto;
+    }
+  }
+
+  footer {
+    text-align: center;
+    a {
+      color: black;
+      font-size: 1em;
     }
   }
 </style>
